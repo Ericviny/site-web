b8d4fff2d9c4d85303601133ef750d8644d332c7
Ericviny <100233203+Ericviny@users.noreply.github.com>
2025-09-29 11:59:37 -0300

    Add files via upload

---
+++ viny/pedrao.js
@@ -0,0 +1,10 @@
+let  taxa  = 10;
+const pi = 3.1416;
+
+console.log("O valor da variavel é: ", taxa);
+
+const prompt = require ('prompt-sync')();
+
+let userName = prompt("Por favor, insira seu nome: ");
+
+console.log("Olá ", userName);
